b46d4f2e45121aa6a7138032cd4e7b38ad793554
Mark Addis <nitro_charlie@live.com>
2022-08-01 11:25:00 +0100

    Small change

--- src/outOfBoundsRead.c
+++ src/outOfBoundsRead.c
@@ -1,33 +1,28 @@
 #include <stddef.h>
 #include <stdio.h>
 
-//Get value before array by not checking the lower bounds
+// Get value before array by not checking the lower bounds
 
-int getValueFromArray(int *array, int len, int index) {
+int getValueFromArray(int *array, int len, int index)
+{
 
-  int value;
+    int value = -1;
 
-  if (index < len) {
-    value = array[index];
-  }
+    if (index < len)
+    {
+        value = array[index];
+    }
 
-  else {
-    printf("Value is: %d\n", array[index]);
-    value = -1;
-  }
-
-  return value;
-
+    return value;
 }
 
 int main(void)
 {
-	int *numbers[5];
-   
+    int *numbers[5];
+
     int result = getValueFromArray(*numbers, 5, -3);
 
     printf("Value before is %d\n", result);
 
-
-	return 0;
+    return 0;
 }
