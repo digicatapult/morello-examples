63f5d5514ee1a72e8403a5f81b77116c7c0a5b6b
Mark Addis <nitro_charlie@live.com>
2022-08-01 11:58:11 +0100

    Updated output

--- src/outOfBoundsRead.c
+++ src/outOfBoundsRead.c
@@ -22,7 +22,13 @@ int main(void)
 
     int result = getValueFromArray(*numbers, 5, -3);
 
-    printf("Value before is %d\n", result);
+    printf("Lower bound pointer: %d, upper bound pointer: %d\n", &numbers[0], &numbers[4]);
+
+    printf("Out of bounds pointer: %d\n", &numbers[-3]);
+
+    printf("Value at first index in array is %d\n", numbers[0]);
+
+    printf("Value 3 indices before array is %d\n", result);
 
     return 0;
 }
