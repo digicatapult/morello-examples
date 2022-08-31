deb4e5d6d4ed395e57747e3b05b30dde1f4e4d34
Jonathan Gray <jonmattgray@gmail.com>
2022-08-31 15:13:58 +0100

    Update out-of-bounds-access to be non-interactive (#9)
    * replace fgets with taking username password pairs as array of arguments
    
    * revert to buffer of 12
    
    * use global pointers for username, password and buffer
    
    * reduce console output and use strcpy
    
    * remove unnecessary else
    
    * bump version

--- src/out-of-bounds-access.c
+++ src/out-of-bounds-access.c
@@ -3,56 +3,66 @@
 #include <string.h>
 #include <stdlib.h>
 
-// An example of a user changing the root password by entering a username that's long enough (>12 characters) to go out of bounds of the allocated input array.
-// The password is replaced with the out of bounds characters e.g. if `root--------123` is inputted for a username, the password is now `123`.
+/*
+    An example of a user changing the root password by attempting to login with a username that's
+    long enough (>16 characters) to go out of bounds of the allocated buffer array.
+    The password is replaced with the out of bounds characters e.g. if `root------------123` is
+    attempted for a username, the password is now `123`.
 
-int checkInput(char input[], char toMatch[])
+    Requires an even number of arguments as 'username password' pairs. Attempts to login with each sequentially.
+    e.g. ./out-of-bounds-access root------------123 bla root 123
+*/
+
+char *username;
+char *usernameBuffer;
+char *password;
+char *passwordBuffer;
+
+int loginAttempt()
 {
-    input[strcspn(input, "\n")] = '\0'; // strip \n from end of fgets
-    if (strcmp(input, toMatch))
+    if (strcmp(usernameBuffer, username))
     {
-        printf("Wrong input\n");
-        return 0;
-    }
-    else
-    {
-        printf("Correct input\n");
+        printf("%s\n", "Login failed");
         return 1;
     }
-}
 
-int runAttempt(char input[], char username[], char password[])
-{
-    printf("\nEnter username: \n");
-    fgets(input, 20, stdin);
-    if (!checkInput(input, username))
+    if (strcmp(passwordBuffer, password))
     {
-        return 0;
+        printf("%s\n", "Login failed");
+        return 1;
     }
 
-    printf("\nEnter password: \n");
-    fgets(input, 20, stdin);
-    return (checkInput(input, password));
+    printf("%s\n", "Login succeeded");
+    return 0;
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    char username[] = "root";
-    char password[] = "password";
+    if ((argc - 1) % 2 != 0) // argc counts program name as first argument
+    {
+        printf("Must be supplied an even number of arguments\n");
+        return 1;
+    }
 
-    char input[12];
-
-    printf("%x\n", &input[sizeof(input)]);
-    printf("%x\n", &password);
+    username = malloc(16 * sizeof(char));
+    usernameBuffer = malloc(16 * sizeof(char));
+    password = malloc(16 * sizeof(char));
+    passwordBuffer = malloc(16 * sizeof(char));
 
     // check memory allocation is adjacent
-    assert((void *)&password[0] == (void *)&input[sizeof(input)]);
+    assert((void *)&password[0] == (void *)&usernameBuffer[16]);
 
-    printf("Password is: %s\n\n", password);
+    // set username and password
+    strcpy(username, "root");
+    strcpy(password, "password");
 
-    while (1)
+    for (int i = 1; i < argc; i += 2)
     {
-        runAttempt(input, username, password);
+        char *usernameAttempt = argv[i];
+        char *passwordAttempt = argv[i + 1];
+        strcpy(usernameBuffer, usernameAttempt);
+        strcpy(passwordBuffer, passwordAttempt);
+        loginAttempt();
     }
 
     return 0;
